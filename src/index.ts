744f6914608b61b884b84203de774111ff0178e1
Hjort <shj@concordium.com>
2023-03-28 13:43:55 +0200

    Export everything from root + export utils

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,2 @@
-export * from './utils/ccd';
-export * from './utils/numberStringHelpers';
-export * from './utils/types';
-export * from './utils/accountHelpers';
-export * from './utils/basicHelpers';
-export * from './utils/hooks';
-export * from './utils/eventHooks';
-export * from './utils/identity/types';
-export * from './utils/timeHelpers';
-export * from './utils/identityHelpers';
-export * from './utils/JSONHelper';
-
+export * from './utils';
 export * from './components';
