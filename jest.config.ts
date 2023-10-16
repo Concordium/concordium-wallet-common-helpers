5ca8e393426acaaf81a477f1b10d8683e313719b
Søren Bruus Zeppelin <soerenbf@gmail.com>
2023-10-16 10:07:02 +0200

    Fix test config

--- jest.config.ts
+++ jest.config.ts
@@ -1,28 +1,14 @@
 /* eslint-disable jest/no-jest-import */
 import type { Config } from 'jest';
 
+const esModules = ['@concordium/web-sdk'].join('|');
+
 const config: Config = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
+    preset: 'ts-jest/presets/js-with-ts-esm',
     moduleNameMapper: {
         '^(\\.\\.?\\/.+)\\.js$': '$1', // Remap esmodule file extensions
     },
-    transformIgnorePatterns: [
-        'node_modules/(?!@concordium/web-sdk)', // @concordium/web-sdk is an ES module only
-    ],
-    moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json'],
-    moduleDirectories: ['node_modules'],
-    setupFiles: ['./test/loadModules.ts'],
-    transform: {
-        '^.+\\.[jt]sx?$': [
-            'ts-jest',
-            {
-                useESM: true,
-                tsconfig: 'tsconfig.json',
-            },
-        ],
-    },
-    extensionsToTreatAsEsm: ['.ts'],
+    transformIgnorePatterns: [`node_modules/(?!${esModules})/`],
 };
 
 export default config;
